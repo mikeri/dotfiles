f3067fedb051349dded6c2e49f9d299fe9632b3c
Michael Ilsaas <mikeri@mikeri.net>
2016-01-14 08:56:55 +0100

    Added pudb custom theme WIP

--- .config/pudb/deserttheme.py
+++ .config/pudb/deserttheme.py
@@ -1,70 +1,122 @@
-palette_dict.update({
-            "variables": ("white", "default"),
+palette.update({
+                "header": ("h235", "h245", "standout"),
 
-            "var label": ("light blue", "default"),
-            "var value": ("white", "default"),
+            # {{{ variables view
+            "variables": ("h235", "h233"),
+            "variable separator": ("h23", "h252"),
 
-            "stack": ("white", "default"),
+            "var label": ("h111", "h233"),
+            "var value": ("h255", "h233"),
+            "focused var label": ("h192", "h24"),
+            "focused var value": ("h192", "h24"),
 
-            "frame name": ("white", "default"),
-            "frame class": ("dark blue", "default"),
-            "frame location": ("light cyan", "default"),
+            "highlighted var label": ("h252", "h22"),
+            "highlighted var value": ("h255", "h22"),
+            "focused highlighted var label": ("h252", "h64"),
+            "focused highlighted var value": ("h255", "h64"),
 
-            "current frame name": (add_setting("white", "bold"), "default"),
-            "current frame class": ("dark blue", "default"),
-            "current frame location": ("light cyan", "default"),
+            "return label": ("h113", "h233"),
+            "return value": ("h113", "h233"),
+            "focused return label": (add_setting("h192", "bold"), "h24"),
+            "focused return value": ("h192", "h24"),
+            # }}}
 
-            "focused frame name": ("black", "dark green"),
-            "focused frame class": (add_setting("white", "bold"), "dark green"),
-            "focused frame location": ("dark blue", "dark green"),
+            # {{{ stack view
+            "stack": ("h235", "h233"),
 
-            "focused current frame name": ("black", "dark green"),
-            "focused current frame class": (add_setting("white", "bold"), "dark green"),
-            "focused current frame location": ("dark blue", "dark green"),
+            "frame name": ("h192", "h233"),
+            "focused frame name": ("h192", "h124"),
+            "frame class": ("h111", "h233"),
+            "focused frame class": ("h192", "h24"),
+            "frame location": ("h252", "h233"),
+            "focused frame location": ("h192", "h24"),
 
-            "breakpoint": ("default", "default"),
+            "current frame name": ("h255", "h22"),
+            "focused current frame name": ("h255", "h64"),
+            "current frame class": ("h111", "h22"),
+            "focused current frame class": ("h255", "h64"),
+            "current frame location": ("h252", "h22"),
+            "focused current frame location": ("h255", "h64"),
+            # }}}
 
-            "search box": ("default", "default"),
+            # {{{ breakpoint view
+            "breakpoint": ("h80", "h233"),
+            "focused breakpoint": ("h192", "h24"),
+            "current breakpoint": (add_setting("h255", "bold"), "h22"),
+            "focused current breakpoint": (add_setting("h255", "bold"), "h64"),
+            # }}}
 
-            "source": ("white", "default"),
-            "highlighted source": ("white", "light cyan"),
-            "current source": ("white", "light gray"),
-            "current focused source": ("white", "brown"),
+            # {{{ ui widgets
 
-            "line number": ("light gray", "default"),
-            "keyword": ("dark magenta", "default"),
-            "name": ("white", "default"),
-            "literal": ("dark cyan", "default"),
-            "string": ("dark red", "default"),
-            "doublestring": ("dark red", "default"),
-            "singlestring": ("light blue", "default"),
-            "docstring": ("light red", "default"),
-            "backtick": ("light green", "default"),
-            "punctuation": ("white", "default"),
-            "comment": ("dark green", "default"),
-            "classname": ("dark cyan", "default"),
-            "funcname": ("white", "default"),
+            "selectable": ("h252", "h235"),
+            "focused selectable": ("h255", "h24"),
 
-            "breakpoint marker": ("dark red", "default"),
+            "button": ("h252", "h235"),
+            "focused button": ("h255", "h24"),
 
-            # {{{ shell
+            "background": ("h245", "h236"),
+            "hotkey": (add_setting("h235", "underline"), "h252", "underline"),
+            "focused sidebar": ("h23", "h252", "standout"),
 
-            "command line edit": ("white", "default"),
-            "command line prompt": (add_setting("white", "bold"), "default"),
+            "warning": (add_setting("h255", "bold"), "h124", "standout"),
 
-            "command line output": (add_setting("white", "bold"), "default"),
-            "command line input": (add_setting("white", "bold"), "default"),
-            "command line error": (add_setting("light red", "bold"), "default"),
+            "label": ("h235", "h252"),
+            "value": ("h255", "h17"),
+            "fixed value": ("h252", "h17"),
+            "group head": (add_setting("h25", "bold"), "h252"),
 
-            "focused command line output": ("black", "dark green"),
-            "focused command line input": (add_setting("white", "bold"), "dark green"),
-            "focused command line error": ("black", "dark green"),
+            "search box": ("h255", "h235"),
+            "search not found": ("h255", "h124"),
 
-            "command line clear button": (add_setting("white", "bold"), "default"),
-            "command line focused button": ("black", "light gray"), # White
-            # doesn't work in curses mode
+            "dialog title": (add_setting("h255", "bold"), "h235"),
 
             # }}}
 
+            # {{{ source view
+            "breakpoint marker": ("h160", "h235"),
+
+            "breakpoint source": ("h252", "h124"),
+            "breakpoint focused source": ("h192", "h124"),
+            "current breakpoint source": ("h192", "h124"),
+            "current breakpoint focused source": (add_setting("h192", "bold"), "h124"),
+            # }}}
+
+            # {{{ highlighting
+            "source": ("h255", "h235"),
+            "focused source": ("h192", "h24"),
+            "highlighted source": ("h252", "h22"),
+            "current source": (add_setting("h252", "bold"), "h23"),
+            "current focused source": (add_setting("h192", "bold"), "h23"),
+            "current highlighted source": ("h255", "h22"),
+
+            "line number": ("h241", "h235"),
+            "keyword": ("h111", "h235"),
+
+            "literal": ("h173", "h235"),
+            "string": ("h113", "h235"),
+            "doublestring": ("h113", "h235"),
+            "singlestring": ("h113", "h235"),
+            "docstring": ("h113", "h235"),
+
+            "name": ("h192", "h235"),
+            "punctuation": ("h223", "h235"),
+            "comment": ("h242", "h235"),
+
+            # }}}
+
+            # {{{ shell
+            "command line edit": ("h255", "h233"),
+            "command line prompt": (add_setting("h192", "bold"), "h233"),
+
+            "command line output": ("h80", "h233"),
+            "command line input": ("h255", "h233"),
+            "command line error": ("h160", "h233"),
+
+            "focused command line output": (add_setting("h192", "bold"), "h24"),
+            "focused command line input": ("h255", "h24"),
+            "focused command line error": ("h235", "h24"),
+
+            "command line clear button": (add_setting("h255", "bold"), "h233"),
+            "command line focused button": ("h255", "h24"),
         })
 
