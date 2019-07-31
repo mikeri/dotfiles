14aae733357124cf0bbddad182655bdb3e415ba7
Michael Ilsaas <mikeri@mikeri.net>
2019-07-31 22:03:50 +0200

    Fix update/apply scripts

--- apply.sh
+++ apply.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Use this to apply updates from the repo to homedir
 
-find . -path ./.git -prune , -type f -a -not -name update.sh -a -not -name apply.sh | while IFS= read -r -d '' file
+find . -path ./.git -prune , -type f -a -not -name update.sh -a -not -name apply.sh -print0 | while IFS= read -r -d '' file
 do
     echo "Copying $file to homedir"
     cp --parents ~/$file .
