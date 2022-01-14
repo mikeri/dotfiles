07462d66a9a8417b1e594f0604385ef80e48672a
Michael Ilsaas <mikeri@mikeri.net>
2022-01-14 14:11:19 +0100

    Fix shellscript preview

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -301,7 +301,7 @@ handle_mime() {
             exit 1;;
 
         ## Text
-        text/* | */xml)
+        text/* | */xml | application/x-shellscript)
             ## Syntax highlight
             if [[ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]]; then
                 exit 2
