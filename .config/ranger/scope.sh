c7b2dc8087879c36a046bec27deaf6f57eef3a6c
Michael Ilsaas <michael@digiflow.no>
2022-10-14 11:35:28 +0200

    Indent XML previews

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -109,7 +109,7 @@ handle_extension() {
         ## JSON
         json)
             size_check 1000000
-            jq --color-output . "${FILE_PATH}" && exit 5
+            jq --color-output . "${FILE_PATH}" | fmt -s -w ${PV_WIDTH} && exit 5
             python -m json.tool -- "${FILE_PATH}" && exit 5
             ;;
 
@@ -305,7 +305,7 @@ handle_mime() {
             exit 1;;
 
         ## Text
-        text/* | */xml | application/x-shellscript)
+        text/* | application/x-shellscript)
             ## Syntax highlight
             if [[ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]]; then
                 exit 2
@@ -324,6 +324,14 @@ handle_mime() {
                 -- "${FILE_PATH}" && exit 5
             pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}"\
                 -- "${FILE_PATH}" && exit 5
+            exit 2;;
+        ## XML
+        application/xml)
+            ## Syntax highlight
+            if [[ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]]; then
+                exit 2
+            fi
+            env COLORTERM=8bit xmllint --format "${FILE_PATH}" | env COLORTERM=8bit bat --color=always --style="plain" && exit 5
             exit 2;;
 
         ## DjVu
