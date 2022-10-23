ec6ee8fa03d2fbc835b07d9d7c4b6731f6fe7d9c
Michael Ilsaas <mikeri@mikeri.net>
2022-10-23 14:43:36 +0200

    Run ssh-add before backupcheck

--- .config/i3/dynamic-config.sh
+++ .config/i3/dynamic-config.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-# xrandr --output DVI-I-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-I-1-1 --mode 1920x1080 --pos 0x0 --rotate normal
-xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 1920x0 --rotate normal
+# export XDG_MENU_PREFIX=i3-
+feh --bg-center ~/Pictures/weyland-yutani.jpg
+alacritty --title "i3float" --option window.dimensions.lines=5 --option window.dimensions.columns=50 -e ssh-add && backupcheck
