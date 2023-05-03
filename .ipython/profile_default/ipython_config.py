362a9db7490efcad26512dff22c3f5299faf5a29
Michael Ilsaas <michael@pursueit.no>
2023-05-03 10:55:08 +0200

    Stop debug messages during completion

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,8 +1,14 @@
+from IPython.core import ultratb
+import logging
+
+logging.getLogger('parso.cache').disabled=True
+logging.getLogger('parso.python.diff').disabled=True
+logging.getLogger('parso.cache.pickle').disabled=True
+
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalIPythonApp.display_banner = False
 c.InteractiveShell.colors = "linux"
 c.TerminalInteractiveShell.highlighting_style = "zenburn"
 c.TerminalInteractiveShell.editing_mode = "vi"
 
-from IPython.core import ultratb
 ultratb.VerboseTB._tb_highlight = "bg:#722 #ff0"
