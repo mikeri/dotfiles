67fa20cbcf9b87010b3ef8d6874409a72780d97b
Michael Ilsaas <mikeri@mikeri.net>
2024-01-17 22:02:28 +0100

    Include polybar ComfyUI script

---
+++ .config/polybar/comfyui-queue.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from urllib import request
+from urllib.error import URLError
+import json
+
+URL = "http://10.0.1.99:8188/queue"
+
+try:
+    response = request.urlopen(URL)
+except URLError:
+    exit()
+response_text = response.read().decode("utf-8")
+data = json.loads(response_text)
+pending = data["queue_pending"]
+length = len(pending)
+print(length)
