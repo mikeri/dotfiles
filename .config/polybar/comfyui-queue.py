7ee38da51244e7afd6bc0138ac4b6299431e4ce0
Michael Ilsaas <mikeri@mikeri.net>
2024-05-13 08:47:20 +0200

    Disable ComfyUI on zero queue

--- .config/polybar/comfyui-queue.py
+++ .config/polybar/comfyui-queue.py
@@ -8,9 +8,13 @@ URL = "http://10.0.1.99:8188/queue"
 try:
     response = request.urlopen(URL)
 except URLError:
+    print()
     exit()
 response_text = response.read().decode("utf-8")
 data = json.loads(response_text)
 pending = data["queue_pending"]
 length = len(pending)
-print(length)
+if length:
+    print(length)
+else:
+    print()
