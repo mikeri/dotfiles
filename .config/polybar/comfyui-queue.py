259ec17fe0907fbedd188f0da494647717eabf47
Michael Ilsaas <mikeri@mikeri.net>
2024-06-08 09:39:40 +0200

    Optimize comfyui queue check

--- .config/polybar/comfyui-queue.py
+++ .config/polybar/comfyui-queue.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 from urllib import request
 from urllib.error import URLError
+from time import sleep
 import json
 
 URL = "http://10.0.1.99:8188/queue"
 
-try:
-    response = request.urlopen(URL)
-except URLError:
-    print()
-    exit()
-response_text = response.read().decode("utf-8")
-data = json.loads(response_text)
-pending = data["queue_pending"]
-length = len(pending)
-if length:
-    print(length)
-else:
-    print()
+while True:
+    sleep(5)
+    try:
+        response = request.urlopen(URL)
+    except URLError:
+        print("", flush=True)
+        continue
+    response_text = response.read().decode("utf-8")
+    data = json.loads(response_text)
+    pending = data["queue_pending"]
+    length = len(pending)
+    if length:
+        print(length, flush=True)
+    else:
+        print("", flush=True)
