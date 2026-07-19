6f3cd34382e9ad51c162721c00d18137a38c20cb
Michael Ilsaas <mikeri@mikeri.net>
2026-07-19 10:14:05 +0200

    Update local i3 config

--- .config/i3/local-startup-pc.sh
+++ .config/i3/local-startup-pc.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
+export GTK_OVERLAY_SCROLLING=0
 # export XDG_MENU_PREFIX=i3-
-xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal
+
+# 1080 ti:
+# xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal
+
+# 4080 super
+xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal
+
+# i3-msg 'workspace "1: one" output HDMI-0'
+# i3-msg 'workspace "2: two" output HDMI-1'
+# K2000:
+# xrandr --output DVI-I-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal
+
+
 setxkbmap no
+xbindkeys
 feh --bg-center ~/Pictures/weyland-yutani.jpg
-alacritty -title "i3float" --option window.dimensions.lines=5 --option window.dimensions.columns=50 -e ssh-add && backupcheck
+alacritty --title "i3float" --option window.dimensions.lines=5 --option window.dimensions.columns=50 -e ssh-add
+# mount /mnt/nas && backupcheck
